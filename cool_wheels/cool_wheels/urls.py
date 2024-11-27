9128905200d7883225b83e61c8a40096a1de5b23
mohaiminul69 <mohaiminul69@gmail.com>
2024-11-27 22:17:49 +0600

    shop page added.

--- cool_wheels/cool_wheels/urls.py
+++ cool_wheels/cool_wheels/urls.py
@@ -2,13 +2,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import home
+from .views import home, shop
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", home, name="homepage"),
+    path("shop/", shop, name="shop"),
     path("brand/<slug:brand_slug>/", home, name="brand_filter"),
+    path("shop/brand/<slug:brand_slug>/", shop, name="shop_brand_filter"),
     path("", include("users.urls")),
     path("", include("cars.urls")),
 ]
--- cool_wheels/cool_wheels/views.py
+++ cool_wheels/cool_wheels/views.py
@@ -11,3 +11,14 @@ def home(request, brand_slug=None):
         cars = Car.objects.filter(brand=brand)
     brands = Brand.objects.all()
     return render(request, "home.html", {"cars": cars, "brands": brands})
+
+
+def shop(request, brand_slug=None):
+    cars = Car.objects.all()
+    if brand_slug is not None:
+        brand = Brand.objects.get(slug=brand_slug)
+        cars = Car.objects.filter(brand=brand)
+    brands = Brand.objects.all()
+    return render(
+        request, "home.html", {"cars": cars, "brands": brands, "type": "shop"}
+    )
